0aeb85c9ac6ced9de25d4e3c91e366b4b3dbd88b
bhavberi <43399374+bhavberi@users.noreply.github.com>
2020-12-12 10:25:10 +0530

    Update 3 12/12/2020

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
-import login
 import csv
 import time
 import os
+
+#Importing the other user-defined modules
+import login
 import quiz_main
 
-def read(level='level -1'):
+def read(level='level -1'): #Function to display the module of the particular level
     with open(level+'.txt','r') as ob:
         print(ob.read())
-    if level=='level -1':
+    if level=='level -1': #For final level
         time.sleep(1)
         quiz_only(level)
         return
-    time.sleep(6)
+    time.sleep(6) #Time given for reading the text
     input("Press any key to continue to the quiz -> ")
-    quiz_only(level)
+    quiz_only(level) #Proceeding for conducting the quiz
 
 def quiz_only(level):
     passpercent=65
     if level=='level -1':
-        passpercent=80
+        passpercent=80 #More Pass Percentage for the final quiz
     quiz_main.main(level[6:],passpercent)
     if quiz_main._pass:
         print("\n\t~~~ Passed !! ~~~")
         if level!='level -1':
-          print("You have passed",level)
-        level_increase()
+            print("You have passed",level)
+        level_increase() #After passing the quiz, increasing the level of the user
         print()
     else:
         print("\n\tFail!! \n\t~~Please Try Again~~")
         menu()
 
-def exit():
+def exit(): #Exit function
     print("\nThanks for using this software.\n\tBy Bhav and Amanvir")
     time.sleep(1.7)
     print("\t\t   ^ ^  ")
@@ -40,35 +42,33 @@ def exit():
     print()
     print("You can download the files from the link\nhttps://github.com/bhavberi/LearningPython\n\tOr\nhttps://repl.it/@bhavberi/LearningPython")
     time.sleep(2.8)
-    os._exit(1)
-def show_all_logindetails():
-  obr=open('Login Details.csv','r')
-  read=csv.reader(obr)
-  print('Serial\tUsername\t Next Level\t Package Type\t Password')
-  print('~~Name \t\tPhone Number\tEmail \t\tCity')
-  count=0
-  for i in read:
-    count+=1
-    print()
-    print('    ',count,'\t',i[0],'\t',i[2],'\t',i[3],'\t',i[1])
-    print('~~',i[4],'\t',i[6],'  \t',i[5],'\t',i[7])
+    os._exit(1) #For closing the program forcefully.
 
-  menu()
+def show_all_logindetails(): #For admin to see the details of all the users enrolled till now.
+    obr=open('Login Details.csv','r')
+    read=csv.reader(obr)
+    print('Serial\tUsername\t Next Level\t Package Type\t Password')
+    print('~~Name \t\tPhone Number\tEmail \t\tCity')
+    count=0
+    for i in read:
+        count+=1
+        print()
+        print('    ',count,'\t',i[0],'\t',i[2],'\t',i[3],'\t',i[1])
+        print('~~',i[4],'\t',i[6],'  \t',i[5],'\t',i[7])
+    menu()
 
-    
-
-def level_increase(): #Max. Level set to 1 till now To be changed afterwards
+def level_increase():
     global username,user_details
     fields=['username','password','level','package','name','email','phone','city']
 
-    obw=open('Login_Details.csv','w',newline='')
+    obw=open('Login_Details.csv','w',newline='') #Another file to which details will be first written and then this file will be renamed.
     data=csv.DictWriter(obw,fieldnames=fields)
 
     obr=open('Login Details.csv','r')
     read=csv.reader(obr)
 
     for i in read:
-        d=dict()
+        d=dict() #Dictionary with the details of users with modified level also
         if i[0].lower()==username:
             level=i[2]
             if level=='Over':
@@ -91,7 +91,7 @@ def level_increase(): #Max. Level set to 1 till now To be changed afterwards
             
             data.writerow(d)
             
-        else:            
+        else:       
             d['username']=i[0]
             d['password']=i[1]
             d['level']=i[2]
@@ -110,18 +110,19 @@ def level_increase(): #Max. Level set to 1 till now To be changed afterwards
         data=csv.reader(ob)
         for i in data:
             if i[0].lower()==username:
-                user_details=i
+                user_details=i #Updating the user details which the program is using.
     menu()
 
-def menu():
+def menu(): #Main menu function from where all other functions are called.
+
     print()
     if user_details[2]=='Over':
-      print("You have completed your course.\nTo restart the course enter 1 or 0\nElse press Enter key")
-      i=input("-> ")
-      if i=='0' or i=='1':
-          level_increase()
-      else:
-          exit()
+        print("You have completed your course.\nTo restart the course enter 1 or 0\nElse press Enter key")
+        i=input("-> ")
+        if i=='0' or i=='1':
+            level_increase()
+        else:
+            exit()
     if user_details=='admin':
         print("To Continue to the module and then quiz, press 1")
         print("To continue to the quiz, press 9")
@@ -132,10 +133,11 @@ def menu():
         print("To continue to the quiz, press 9")
     print("Else press any other key to exit")
     ch=input("-> ")
+    
     if ch.isdigit():
-      choice=int(ch)
+        choice=int(ch)
     else:
-      choice=0
+        choice=0
     if choice==0:
         exit()
     elif choice==1:
@@ -149,51 +151,58 @@ def menu():
             level_ask='level '+input("Enter number of level to see(For final, enter -1) : ")
             quiz_only(level_ask)
         else:
-            quiz_only(user_details[2])
+            if user_details[2]=='level -1':
+                read(user_details[2])
+            else:
+                quiz_only(user_details[2])
     elif choice==5:
-      show_all_logindetails()
+        show_all_logindetails()
     else:
-      exit()
+        exit()
+
 def welcome():
-  print()
-  time.sleep(0.5)
-  print('\t\tA')
-  time.sleep(1.5)
-  print('\t\t\t&')
-  time.sleep(1.5)
-  print('\t\t\t\tB')
-  time.sleep(1.5)
-  print('\t\tSoftwares')
-  time.sleep(0.7)
-  print()
-  q=0
-  while q<29:
-    print('-~',end='')
-    q+=1
-  time.sleep(1.5)
-  print('\n')
-  time.sleep(1.5)
-  print("This software is developed by 'Bhav Beri' & 'Amanvir Singh'\n")
-  time.sleep(0.9)
-  q=0
-  while q<29:
-    print('-~',end='')
-    q+=1
-  time.sleep(1.5)
-  print('\n')
-  time.sleep(1.5)
-            
+    print()
+    time.sleep(0.5)
+    print('\t\tA')
+    time.sleep(1.5)
+    print('\t\t\t&')
+    time.sleep(1.5)
+    print('\t\t\t\tB')
+    time.sleep(1.5)
+    print('\t\tSoftwares')
+    time.sleep(0.7)
+    print()
+    q=0
+    while q<29:
+      print('-~',end='')
+      q+=1
+    time.sleep(1.5)
+    print('\n')
+    time.sleep(1.5)
+    print("This software is developed by 'Bhav Beri' & 'Amanvir Singh'\n")
+    time.sleep(0.9)
+    q=0
+    while q<29:
+      print('-~',end='')
+      q+=1
+    time.sleep(1.5)
+    print('\n')
+    time.sleep(1.5)
+
+#Start of the execution of the main file            
 welcome()
 
+#Calling login module and checking the initials of the user
 login.login()
 username=login.username()
 user_details=''
-if username=='adminab' or username=='computer':
+if username=='adminab' or username=='computer':#For admin
     user_details='admin'
 with open('Login Details.csv','r') as ob:
     data=csv.reader(ob)
     for i in data:
         if i[0].lower()==username:
             user_details=i
-  
+
+#After logining in of the user, start of the main module for the quizzes and the learning part. 
 menu()
