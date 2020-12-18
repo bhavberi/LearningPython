4d80bd8e33e6926432c5bd29051b7e4f36450138
bhavberi <43399374+bhavberi@users.noreply.github.com>
2020-12-18 19:57:21 +0530

    Update 4 18/12/2020
    Added Update Password Option, And link to github and repl.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import csv
 import time
 import os
+import webbrowser
+import warnings
+from getpass import getpass
+
+warnings.simplefilter("error", category=Warning)
 
 #Importing the other user-defined modules
 import login
@@ -56,6 +61,43 @@ def show_all_logindetails(): #For admin to see the details of all the users enro
         print('    ',count,'\t',i[0],'\t',i[2],'\t',i[3],'\t',i[1])
         print('~~',i[4],'\t',i[6],'  \t',i[5],'\t',i[7])
     menu()
+
+def web():
+    print()
+    print("For Github link(For the Source Code), press 1")
+    print("For repl.it link(For online interpreter), press 2")
+    ch=int(input('-> '))
+    print()
+    if ch==1:
+        print('https://github.com/bhavberi/LearningPython')
+        print('\nThis is the Github link, either copy it and then paste in your browser,\nOr enter 9 to open the link directly in your browser.\nPress any other key to go back to menu.')
+        ch1=input('-> ')
+        if ch1=='9':
+            webbrowser.open_new('https://github.com/bhavberi/LearningPython')
+            time.sleep(3.1)
+            input('To continue back, press enter key.')
+            menu()
+            return
+        else:
+            menu()
+            return
+    elif ch==2:
+        print('https://repl.it/@bhavberi/LearningPython')
+        print('\nThis is the Repl link, either copy it and then paste in your browser,\nOr enter 9 to open the link directly in your browser.\nPress any other key to go back to menu.')
+        ch1=input('-> ')
+        if ch1=='9':
+            webbrowser.open_new('https://repl.it/@bhavberi/LearningPython')
+            time.sleep(3.1)
+            input('To continue back, press enter key.')
+            menu()
+            return
+        else:
+            menu()
+            return
+    else:
+        print("Wrong Entry.")
+        menu()
+        return
 
 def level_increase():
     global username,user_details
@@ -113,14 +155,87 @@ def level_increase():
                 user_details=i #Updating the user details which the program is using.
     menu()
 
+def update_password():
+    global username,user_details
+    fields=['username','password','level','package','name','email','phone','city']
+
+    obw=open('Login_Details.csv','w',newline='') #Another file to which details will be first written and then this file will be renamed.
+    data=csv.DictWriter(obw,fieldnames=fields)
+
+    obr=open('Login Details.csv','r')
+    read=csv.reader(obr)
+
+    for i in read:
+        d=dict() #Dictionary with the details of users with modified level also
+        if i[0].lower()==username:
+
+            while True:
+                try:
+                  password=getpass("Enter password(min. 6 digits long) (Secured entry) -> ")
+                except:
+                  password=input("Enter password(min. 6 digits long) -> ")
+                if len(password)<6:
+                    print("\nPassword must be atleast 6 letters long")
+                    continue
+                print('\nEntered Password -> ',end='')
+                print('*'*len(password))
+                try:
+                  cpass=getpass("Confirm you password (Secured entry) -> ")
+                except:
+                  cpass=input("Confirm you password -> ")
+                if cpass==password:
+                    break
+                print("\nOriginal and Confirmatory Password's doesn't match\nPlease enter again.")
+                ch=input('To try again, press 1\nElse press enter key')
+                if ch!='1':
+                  menu()
+                  return
+            
+            d['username']=i[0]
+            d['password']=password+'#ab'
+            d['level']=i[2]
+            d['package']=i[3]
+            d['name']=i[4]
+            d['email']=i[5]
+            d['phone']=i[6]
+            d['city']=i[7]
+            
+            data.writerow(d)
+            
+        else:       
+            d['username']=i[0]
+            d['password']=i[1]
+            d['level']=i[2]
+            d['package']=i[3]
+            d['name']=i[4]
+            d['email']=i[5]
+            d['phone']=i[6]
+            d['city']=i[7]
+            
+            data.writerow(d)
+    obw.close()
+    obr.close()    
+    os.remove('Login Details.csv')
+    os.rename('Login_Details.csv','Login Details.csv')
+    with open('Login Details.csv','r') as ob:
+        data=csv.reader(ob)
+        for i in data:
+            if i[0].lower()==username:
+                user_details=i #Updating the user details which the program is using.
+    menu()
+
 def menu(): #Main menu function from where all other functions are called.
 
     print()
     if user_details[2]=='Over':
-        print("You have completed your course.\nTo restart the course enter 1 or 0\nElse press Enter key")
+        print("You have completed your course.\n\nTo restart the course enter 1 or 0\nElse press Enter key")
+        print("To update your passord, press 2\nElse press Enter key")
         i=input("-> ")
         if i=='0' or i=='1':
             level_increase()
+        elif i=='2':
+            update_password()
+            menu()
         else:
             exit()
     if user_details=='admin':
@@ -129,34 +244,52 @@ def menu(): #Main menu function from where all other functions are called.
         print("To see the login details of all the users till now, press 5")
     elif user_details[3]=='Complete':
         print("To Continue to the module and then quiz, press 1")
+        print("To update your passord, press 2")
     else:
         print("To continue to the quiz, press 9")
+        print("To update your passord, press 2")
+    print("For Source File/Online Interpreter of this program, press 0")
     print("Else press any other key to exit")
-    ch=input("-> ")
+    ch=input("-> ") #Input of the choice of the user
     
     if ch.isdigit():
         choice=int(ch)
     else:
-        choice=0
-    if choice==0:
+        choice=-1
+    
+    if choice==-1:
         exit()
+    elif choice==0:
+        web()
+    elif choice==2:
+        if user_details!='admin':
+            update_password()
+        else:
+            exit()
     elif choice==1:
         if user_details=='admin':
             level_ask='level '+input("Enter number of level to see(For final, enter -1) : ")
             read(level_ask)
         else:
+            if user_details[3]=="Quiz":
+                exit()
             read(user_details[2])
     elif choice==9:
         if user_details=='admin':
             level_ask='level '+input("Enter number of level to see(For final, enter -1) : ")
             quiz_only(level_ask)
         else:
+            if user_details[3]=='Complete':
+                exit()
             if user_details[2]=='level -1':
                 read(user_details[2])
             else:
                 quiz_only(user_details[2])
     elif choice==5:
-        show_all_logindetails()
+        if user_details=='admin':
+            show_all_logindetails()
+        else:
+            exit()
     else:
         exit()
 
